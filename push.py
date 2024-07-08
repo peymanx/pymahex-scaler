2fb69471176bc22b992511f589bb63c4f3c9e244
Peymanx <peyman.majidi@outlook.com>
2024-07-08 17:09:05 +0430

    fg

--- push.py
+++ push.py
@@ -6,9 +6,13 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin, GPIO.IN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
+white_pressed = False
 
 while True: # Run forever
-    if GPIO.input(pin) == GPIO.LOW:
+    if GPIO.input(pin) == GPIO.LOW and white_pressed == False:
         print("Button WHITE was pushed!")
+        white_pressed = True
+    else:
+        white_pressed == False
 
 
