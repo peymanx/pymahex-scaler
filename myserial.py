4cccac794bd24329f1351ea6061838e4da7feb9c
Peymanx <peyman.majidi@outlook.com>
2024-07-08 18:12:25 +0430

    gdg

--- myserial.py
+++ myserial.py
@@ -5,7 +5,7 @@ import serial
 
 
 ser = serial.Serial(
-        port='/dev/tty1', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
+        port='/dev/ttyS0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
         baudrate = 9600,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
