0252b8eadc70ef3956e3fd4f4e808d6cd59a71ac
Peymanx <peyman.majidi@outlook.com>
2024-07-08 17:12:45 +0430

    time.sleep(0.1)

--- push.py
+++ push.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-
+import time
 
 pin = 40
 GPIO.setwarnings(False) # Ignore warning for now
@@ -12,6 +12,7 @@ while True: # Run forever
     if GPIO.input(pin) == GPIO.LOW and white_pressed == False:
         print("Button WHITE was pushed!")
         white_pressed = True
+        time.sleep(0.1)
 
 
 
