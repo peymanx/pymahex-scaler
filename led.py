80d1ee0b49af7bfe09ce1195b982be3bcb53e1bb
Peymanx <peyman.majidi@outlook.com>
2024-07-08 16:33:00 +0430

    led accepts arg

--- led.py
+++ led.py
@@ -1,7 +1,18 @@
 import RPi.GPIO as gpio
 import time
+import sys
+
+
+
+print('Led Tester')
 
 pin = 7
+
+if len(sys.argv>1):
+        pin = sys.argv[1]
+
+
+print('Tesing pin %d' % pin)
 
 gpio.setmode(gpio.BCM)
 gpio.setup(pin, gpio.OUT)
