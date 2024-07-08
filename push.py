dd6c5c6af1da8c31ff2f9da07f14deb7ef3fe93c
Peymanx <peyman.majidi@outlook.com>
2024-07-08 17:06:00 +0430

    pin

--- push.py
+++ push.py
@@ -1,11 +1,14 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
+
+pin = 40
 GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setup(21, GPIO.IN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin, GPIO.IN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 
 while True: # Run forever
-    if GPIO.input(21) == GPIO.LOW:
+    if GPIO.input(pin) == GPIO.LOW:
         print("Button WHITE was pushed!")
 
 
