cd1d41f44a7d731912852c23d17a05c83e5c997e
Peymanx <peyman.majidi@outlook.com>
2024-07-09 12:54:19 +0430

    fgd

--- test.py
+++ test.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import serial
-test_string = "hello jkfchdshfjk hsdfjk hjkfh jkshfjk sfhjk shfkjhs jkfhskj fhkjshfjkshjfkhsjkfhjksfhj".encode('utf-8')
-port_list = ["/dev/ttyAMA0","/dev/ttyAMA0","/dev/ttyS0","/dev/ttyS"]
-for port in port_list:
-  try:
+import serial, time
+test_string = "hello 123kg".encode('utf-8')
+port= "/dev/ttyS0"
+
+while True:
     serialPort = serial.Serial(port, 9600, timeout = 2)
     print ("Serial port", port, " ready for test :")
     bytes_sent = serialPort.write(test_string)
     print ("Sended", bytes_sent, "byte")
-    loopback = serialPort.read(bytes_sent)
-    if loopback == test_string:
-      print ("Received ",len(loopback), "bytes. Port", port,"is OK ! \n")
-    else:
-      print ("Received incorrect data:", loopback, "on serial part", port, "loopback \n")
-    serialPort.close()
-  except IOError:
-    print ("Error on", port,"\n")
+    time.sleep(0.5)
+
