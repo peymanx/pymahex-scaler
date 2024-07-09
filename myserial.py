6450519e16c558d86791203e7f9390bffb26d4bf
Peymanx <peyman.majidi@outlook.com>
2024-07-09 10:57:04 +0430

    new serial

--- myserial.py
+++ myserial.py
@@ -5,20 +5,30 @@ def connect(port):
 
 ser = connect('/dev/ttyS0')
 buf = bytearray()
-
+print('here')
 while True:
-    print('.', end='')
-    num_bytes = max(1, min(1024, ser.in_waiting))
+    #print('.')
+    num_bytes = 1
+    num_bytes = max(100, min(1024, ser.in_waiting))
+    print(num_bytes)
     data = ser.read(num_bytes)
+    print('data: ' + str(data))
     if data:
         buf.extend(data)
-        print(data)
-        print(buf)
     else:
         # no data to read aka disconnection has occurred
+        #print('else')
         ser = connect('/dev/ttyS0')
         if ser.isOpen():
+            #print('open')
             continue
         else:
             # could not reconnect
+            print('failto connect')
             break
+
+
+
+
+
+
