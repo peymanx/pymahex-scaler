7da27ec404578d2603552d15b9854012131e2ff1
Peymanx <peyman.majidi@outlook.com>
2024-07-09 17:46:40 +0430

    dffsf

--- push_buttons.py
+++ push_buttons.py
@@ -24,30 +24,35 @@ def wait_for_next():
 while True: # Run forever
 
     # white button
-    if GPIO.input(pin_white) == GPIO.LOW and white_pressed == False:
-        print("@ White button is pressed")
-        white_pressed = True
-        wait_for_next()
-        
-    if GPIO.input(pin_white) == GPIO.HIGH:
-        white_pressed = False
+    try:
+        if GPIO.input(pin_white) == GPIO.LOW and white_pressed == False:
+            print("@ White button is pressed")
+            white_pressed = True
+            wait_for_next()
+            
+        if GPIO.input(pin_white) == GPIO.HIGH:
+            white_pressed = False
 
-    # red button
-    if GPIO.input(pin_red) == GPIO.LOW and red_pressed == False:
-        print("@ Red Button is pressed")
-        red_pressed = True
-        wait_for_next()
+        # red button
+        if GPIO.input(pin_red) == GPIO.LOW and red_pressed == False:
+            print("@ Red Button is pressed")
+            red_pressed = True
+            wait_for_next()
 
-    if GPIO.input(pin_red) == GPIO.HIGH:
-        red_pressed = False
+        if GPIO.input(pin_red) == GPIO.HIGH:
+            red_pressed = False
 
-    # green button
-    if GPIO.input(pin_green) == GPIO.LOW and green_pressed == False:
-        print("@ Green Button is pressed")
-        green_pressed = True
-        wait_for_next()
+        # green button
+        if GPIO.input(pin_green) == GPIO.LOW and green_pressed == False:
+            print("@ Green Button is pressed")
+            green_pressed = True
+            wait_for_next()
 
-    if GPIO.input(pin_green) == GPIO.HIGH:
-        green_pressed = False
+        if GPIO.input(pin_green) == GPIO.HIGH:
+            green_pressed = False
+    except KeyboardInterrupt:
+        print('\r\nbye bye!')
+        break
+
 
 
