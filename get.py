b89f83ee870fd680d354e973de40fc70f6dd630e
Peymanx <peyman.majidi@outlook.com>
2024-07-09 11:57:19 +0430

    ff

--- get.py
+++ get.py
@@ -4,7 +4,7 @@ import serial
 
 port = "/dev/ttyS0"
 bytes_sent = 12
-serialPort = serial.Serial(port, 9600, timeout = 2)
+serialPort = serial.Serial(port, 9600, timeout = 1)
 
 while True:
     print("...")
