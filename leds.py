8f3fc1df079733196e741afc9489f45661f50296
Peymanx <peyman.majidi@outlook.com>
2024-07-09 16:06:53 +0430

    fsf

--- leds.py
+++ leds.py
@@ -18,9 +18,11 @@ gpio.setup(pin_error, gpio.OUT)
 
 def turn_on(pin):
      gpio.output(pin, gpio.HIGH)
+     print("Turning on " + str(pin))
 
 def turn_off(pin):
      gpio.output(pin, gpio.LOW)
+     print("Turning off " + str(pin))
 
 if len(sys.argv)>2:
     match sys.argv[0]:
