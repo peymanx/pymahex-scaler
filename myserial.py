b9d568ddffc203479615f6166b27023dd0a22a72
Peymanx <peyman.majidi@outlook.com>
2024-07-09 10:59:42 +0430

    .decode("utf-8")

--- myserial.py
+++ myserial.py
@@ -7,23 +7,17 @@ ser = connect('/dev/ttyS0')
 buf = bytearray()
 print('here')
 while True:
-    #print('.')
-    num_bytes = 1
     num_bytes = max(100, min(1024, ser.in_waiting))
-    print(num_bytes)
+    print(num_bytes.decode("utf-8"))
     data = ser.read(num_bytes)
     print('data: ' + str(data))
     if data:
         buf.extend(data)
     else:
-        # no data to read aka disconnection has occurred
-        #print('else')
         ser = connect('/dev/ttyS0')
         if ser.isOpen():
-            #print('open')
             continue
         else:
-            # could not reconnect
             print('failto connect')
             break
 
