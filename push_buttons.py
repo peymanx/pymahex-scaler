cb2814e74ff27df6e51199776b0362096cc56f3c
Peymanx <peyman.majidi@outlook.com>
2024-07-09 15:49:56 +0430

    wait_for_next():

--- push_buttons.py
+++ push_buttons.py
@@ -17,13 +17,18 @@ white_pressed = False
 green_pressed = False
 red_pressed = False
 
+
+def wait_for_next():
+    time.sleep(0.2)
+
 while True: # Run forever
 
     # white button
     if GPIO.input(pin_white) == GPIO.LOW and white_pressed == False:
         print("@ Button White is pressed")
         white_pressed = True
-        time.sleep(0.5)
+        wait_for_next():
+        
     if GPIO.input(pin_white) == GPIO.HIGH:
         white_pressed = False
 
@@ -31,7 +36,8 @@ while True: # Run forever
     if GPIO.input(pin_red) == GPIO.LOW and red_pressed == False:
         print("@ Button Red is pressed")
         red_pressed = True
-        time.sleep(0.5)
+        wait_for_next():
+
     if GPIO.input(pin_red) == GPIO.HIGH:
         red_pressed = False
 
@@ -39,7 +45,8 @@ while True: # Run forever
     if GPIO.input(pin_green) == GPIO.LOW and green_pressed == False:
         print("@ Button Green is pressed")
         green_pressed = True
-        time.sleep(0.5)
+        wait_for_next():
+
     if GPIO.input(pin_green) == GPIO.HIGH:
         green_pressed = False
 
