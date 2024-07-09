75c03a4521bee8cff8a94bff30466a83a4ed31ea
Peymanx <peyman.majidi@outlook.com>
2024-07-09 15:16:49 +0430

    dsadsd

--- get.py
+++ get.py
@@ -4,7 +4,7 @@ import serial
 
 port = "/dev/ttyS0"
 bytes_sent = 24
-serialPort = serial.Serial(port, 9600, timeout = 1)
+serialPort = serial.Serial(port, 9600, timeout = 0.1)
 buffer = ''
 
 def normalize(s):
@@ -18,10 +18,10 @@ while True:
     dot = buffer.rfind('.')
     if wn != -1 and  kg != -1 and dot != -1:
         weight = normalize(buffer[wn+2:kg])
+        buffer=''
         try:
             print(float(weight))
         except:
             ...
         
-        buffer=''
 
