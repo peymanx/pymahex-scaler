18f90cfed615d1c21db4d58de4bb1bcc6629d9f0
Peymanx <peyman.majidi@outlook.com>
2024-07-09 17:44:17 +0430

    fdg

--- push_buttons.py
+++ push_buttons.py
@@ -1,7 +1,6 @@
 import RPi.GPIO as GPIO 
 import time
 import sys
-import buzzer
 
 pin_white = 40
 pin_red = 12
@@ -28,9 +27,7 @@ while True: # Run forever
     if GPIO.input(pin_white) == GPIO.LOW and white_pressed == False:
         print("@ White button is pressed")
         white_pressed = True
-        buzzer.play(0.1)
         wait_for_next()
-
         
     if GPIO.input(pin_white) == GPIO.HIGH:
         white_pressed = False
@@ -39,7 +36,6 @@ while True: # Run forever
     if GPIO.input(pin_red) == GPIO.LOW and red_pressed == False:
         print("@ Red Button is pressed")
         red_pressed = True
-        buzzer.play(0.1)
         wait_for_next()
 
     if GPIO.input(pin_red) == GPIO.HIGH:
@@ -49,7 +45,6 @@ while True: # Run forever
     if GPIO.input(pin_green) == GPIO.LOW and green_pressed == False:
         print("@ Green Button is pressed")
         green_pressed = True
-        buzzer.play(0.1)
         wait_for_next()
 
     if GPIO.input(pin_green) == GPIO.HIGH:
