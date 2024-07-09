b523ce99b09974ca6e4a41bc6c1705659764ef1b
Peymanx <peyman.majidi@outlook.com>
2024-07-09 11:49:19 +0430

    get

--- get.py
+++ get.py
@@ -1,17 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import serial
-serialData = serial.Serial('/dev/ttyACM0',115200) 
-
-try:    
-    # Body of data acquisition code
-    while True:
-        line = serialData.read(12); # stream is 325 bytes long ,  last byte is a 0x011
-        # first 4 bytes are time in msec
-        Ttemp = array.array("I",line[0:4]);  # extracts time the I, uint32_t millis
-        currT = Ttemp[0]/1000.0; # writes single element array to variable
-        print "Time = ",currT
-        # extracts the array of h, int16_t variable
-        tempArray = array.array("h",line[4:324]); s
-        print "tempArray ="
-        print tempArray
-except:
-    ...
+test_string = "Test serial port ...".encode('utf-8')
+port_list = ["/dev/ttyAMA0","/dev/ttyAMA0","/dev/ttyS0","/dev/ttyS"]
+for port in port_list:
+  try:
+    serialPort = serial.Serial(port, 9600, timeout = 2)
+    print ("Serial port", port, " ready for test :")
+    bytes_sent = serialPort.write(test_string)
+    print ("Sended", bytes_sent, "byte")
+    loopback = serialPort.read(bytes_sent)
+    if loopback == test_string:
+      print ("Received ",len(loopback), "bytes. Port", port,"is OK ! \n")
+    else:
+      print ("Received incorrect data:", loopback, "on serial part", port, "loopback \n")
+    serialPort.close()
+  except IOError:
+    print ("Error on", port,"\n")
