7e614ac26b21dd591b9a4b6f335dfdf1ebd8c9f3
Peymanx <peyman.majidi@outlook.com>
2024-07-09 14:01:14 +0430

    fdsg

--- get.py
+++ get.py
@@ -7,13 +7,22 @@ bytes_sent = 24
 serialPort = serial.Serial(port, 9600, timeout = 1)
 buffer = ''
 
+
+
+def remove(s):
+    s = s.replace(" ", "")
+    s = s.replace("\r", "")
+    s = s.replace("\n", "")
+    return s
+
 while True:
     data = serialPort.read(bytes_sent)
     buffer+=data.decode()
     wn = buffer.find('wn') 
     kg = buffer.find('kg')
     if wn != -1 and  kg != -1:
-        weight = float(buffer[wn+2:kg])
-        print(weight)
+        weight = remove(buffer[wn+2:kg])
+        if len(weight) == 6:
+            print(float(weight) + "kg")
         buffer=''
 
