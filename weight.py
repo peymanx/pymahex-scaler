6e16defe39815b4fe5b3176b9f6e56958b9cd20b
Peymanx <peyman.majidi@outlook.com>
2024-07-10 12:02:38 +0430

    ljl

--- weight.py
+++ weight.py
@@ -28,7 +28,7 @@ while True:
                 fweight = float(weight)
                 if last != fweight:
                     os.system('clear')
-                    print(f"{f.renderText( str(fweight)):2.2f}kg")
+                    print(f"{f.renderText(fweight):2.2f}kg")
                     last = fweight
             except:
                 ...
