bfd85a5cec4928d52260809d1d84bf69857971dd
Peymanx <peyman.majidi@outlook.com>
2024-07-10 12:46:54 +0430

    post

---
+++ post.py
@@ -0,0 +1,29 @@
+import sys
+
+if len(sys.argv)>1:
+    barcode = sys.argv[1]
+    print(f"""
+    [
+        {
+            "parcelId": "{barcode}",
+            "length": 1,
+            "width": 2,
+            "height": 3,
+            "grossWeight": 1.0,
+            "scannedOn": "2024-04-22T10:32:00"
+        }
+    ]  sent!""")
+    if barcode == 'SHP3352120467-1':    
+        print("""
+        Response: 
+        [
+            {
+                "parcelId": "{barcode}",
+                "cityCode": "IR-ZHDN"
+            }
+        ]
+            
+        """)
+else:
+    print("[]")
+
