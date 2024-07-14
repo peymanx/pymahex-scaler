2b7eb8f601faf9f980cd81d21901879391c0fe23
Peymanx <peyman.majidi@outlook.com>
2024-07-14 13:30:11 +0430

    gdg

--- buzzer.py
+++ buzzer.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as gpio
+import RPi.gpio as gpio
 import time, sys
 
 pin = 37
@@ -6,22 +6,39 @@ pin = 37
 gpio.setmode(gpio.BOARD)
 gpio.setup(pin, gpio.OUT)
 
-# Buzzer
-def play(delay=0.5):
-    gpio.output(pin, gpio.HIGH)
-    print('Buzzer beeeeeeeeeeeez')
-    time.sleep(delay)
-    silent()
 
-def silent():
-    gpio.output(pin, gpio.LOW)
-    print('Buzzer off')
+p = gpio.PWM(12, 100)
 
+c4 = 261
+d4 = 294
+e4 = 329
+f4 = 349
+g4 = 392
+a4 = 440
+b4 = 493
+c5 = 523.25
 
-if __name__ == '__main__':
-    delay = 0.5
+speed = 0.1
 
-    if len(sys.argv)>1:
-            delay = float(sys.argv[1])
-    play(delay)
-    silent()
+gpio.output(12, True) 
+p.start(10) # 10% duty cycle sounds 'ok'
+
+p.ChangeFrequency(c4)
+time.sleep(speed)
+p.ChangeFrequency(d4)  
+time.sleep(speed)
+p.ChangeFrequency(e4)   
+time.sleep(speed)
+p.ChangeFrequency(f4)  
+time.sleep(speed)
+p.ChangeFrequency(g4)    
+time.sleep(speed)
+p.ChangeFrequency(a4)    
+time.sleep(speed)
+p.ChangeFrequency(b4)    
+time.sleep(speed)
+p.ChangeFrequency(c5)    
+time.sleep(speed)
+
+p.stop()
+gpio.cleanup()
