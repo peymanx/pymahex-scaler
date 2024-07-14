6921c67267f634d30766bd1569b8245af38db776
Peymanx <peyman.majidi@outlook.com>
2024-07-14 18:32:01 +0430

    api

--- api.py
+++ api.py
@@ -1,10 +1,7 @@
-from color import * 
-import requests
-import json, sys
-import datetime
+import requests,json, sys,datetime
 from pathlib import Path
-from color import *
 from enum import Enum
+from color import *
 
 class ApiResult(Enum):
     TEHRAN = 1
@@ -35,6 +32,12 @@ def send_to_ecourier(barcode, weight):
         "scannedOn": now
     }
     ])
+    
+    json_object = json.loads(payload)
+    json_formatted_str = json.dumps(json_object, indent=2)
+    print(json_formatted_str)
+    
+    
     headers = {
     'authorization': 'Basic amFsSkh6VlN2QnlNQkNXRnNNcVJVZjdQU3lDQ2ZhZExBNU51bmlIc3dFNGVlcVpyOg==',
     'Content-Type': 'application/json'
@@ -45,6 +48,9 @@ def send_to_ecourier(barcode, weight):
 
         print(response.text)
         if response.status_code == 200 and response.text.__contains__(barcode):
+            json_object = json.loads(response.text)
+            json_formatted_str = json.dumps(json_object, indent=2)
+            print(json_formatted_str)
             if is_tehran(response.text):
                 print('Tehran', color=print.HIGHLIGHTED_GREEN)
                 return ApiResult.TEHRAN
