f22c145888f41ab938e71854e71526200d8b8d8a
Peymanx <peyman.majidi@outlook.com>
2024-07-14 16:19:59 +0430

    real

--- post.py
+++ post.py
@@ -1,25 +1,31 @@
-import sys
 from color import * 
 import requests
 import json
+import datetime
+
 
 url = "http://172.24.24.59:8011/v1/parcels/scan"
 
-payload = json.dumps([
-  {
-    "parcelId": "SHP3352120467-1",
-    "length": 1,
-    "width": 2,
-    "height": 3,
-    "grossWeight": 1,
-    "scannedOn": "2024-04-22T10:32:00"
-  }
-])
-headers = {
-  'authorization': 'Basic amFsSkh6VlN2QnlNQkNXRnNNcVJVZjdQU3lDQ2ZhZExBNU51bmlIc3dFNGVlcVpyOg==',
-  'Content-Type': 'application/json'
-}
+def get_parcel(barcode, weight):
+    now = datetime.datetime.utcnow().isoformat()
+    payload = json.dumps([
+    {
+        "parcelId": barcode,
+        "length": 0,
+        "width": 0,
+        "height": 0,
+        "grossWeight": weight,
+        "scannedOn": now
+    }
+    ])
+    headers = {
+    'authorization': 'Basic amFsSkh6VlN2QnlNQkNXRnNNcVJVZjdQU3lDQ2ZhZExBNU51bmlIc3dFNGVlcVpyOg==',
+    'Content-Type': 'application/json'
+    }
 
-response = requests.request("POST", url, headers=headers, data=payload)
+    response = requests.request("POST", url, headers=headers, data=payload)
 
-print(response.text)
+    print(response.text)
+
+
+get_parcel('SHP3352120467-1', 5)
