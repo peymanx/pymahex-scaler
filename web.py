2c66ece0c96b435624c01632cc444c0c55603259
PeymanX <Peyman.majidi@outlook.com>
2024-07-15 17:54:16 +0330

    app

--- web.py
+++ web.py
@@ -1,12 +1,9 @@
 from flask import Flask
-
-app = Flask(__name__)
-
+app = Flask('RSA Scaler')
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def root():
+   return 'Hello'
 
-
-while True:
-    pass
+if __name__ == '__main__':
+   app.run()
