f9561f3002698d36f7066476d2d34c9197c3a2b8
Peymanx <peyman.majidi@outlook.com>
2024-09-01 13:29:17 +0430

    test again

--- serial_tester.py
+++ serial_tester.py
@@ -1,4 +1,4 @@
-import serial_tester, os
+import serial, os
 
 
 
@@ -18,7 +18,7 @@ def listen():
     global  current_weight, last
     port = "/dev/ttyS0"
     bytes_sent = 24
-    serialPort = serial_tester.Serial(port, 9600, timeout = 0.01)
+    serialPort = serial.Serial(port, 9600, timeout = 0.01)
     buffer = ''     
   
     while True:
