ec58833afe98508e4a2ce9b8426aa8fcb3b19e57
Peymanx <peyman.majidi@outlook.com>
2024-09-01 13:18:39 +0430

    serila tester added

---
+++ serial.py
@@ -0,0 +1,31 @@
+import serial, os
+
+
+
+global  current_weight
+
+last =-1.0
+current_weight = 0.0
+
+
+def normalize(s):
+    return s.replace(" ", "").replace("\r", "").replace("\n", "")
+
+def get():
+    return current_weight
+
+def listen():    
+    global  current_weight, last
+    port = "/dev/ttyS0"
+    bytes_sent = 24
+    serialPort = serial.Serial(port, 9600, timeout = 0.01)
+    buffer = ''     
+  
+    while True:
+
+        data = serialPort.read(bytes_sent)
+        buffer+=data.decode()
+        print(buffer)
+
+
+listen()
